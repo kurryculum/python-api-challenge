54a383ad975989209536fbc1a171da75d347d743
vineet <vineetsikri@yahoo.com>
2020-08-29 15:27:48 -0500

    add

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d51879dc8581dbf6ee43ee85a3a06d04"
 
-# Google API Key
-g_key = "YOUR KEY HERE!"
+# Google 
+g_key="AIzaSyBgRm_boe7_GjX_LLR4g0hR93yTc_w-FFQ"
